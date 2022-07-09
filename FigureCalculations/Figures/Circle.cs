5739f41c3bcf485d85ce6f61708e35dfd19ed13b
Vladimir <gna.vladimir@gmail.com>
2022-07-10 02:27:50 +0900

    Add project files.

---
+++ FigureCalculations/Figures/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculations.Figures
+{
+    public class Circle : Shape
+    {
+        private readonly double _radius;
+        public override string? Name => _name;
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius), "Radius can't be less than or equal to zero");
+            }
+            _name = "Circle";
+            _radius = radius;
+        }
+        public override double GetShapeArea()
+        {
+            return Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+}
